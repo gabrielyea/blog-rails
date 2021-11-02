d80276d9afc691f2988f2b2f93b1a06c2e9c1916
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-02 09:50:36 -0600

    Git add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'pages/hello'
+  root 'users#index'
+  get "/users/", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
