cc6d5bc39424b0962e287d594cd06807d5169f77
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-02 10:11:07 -0600

    Add routes to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'users/index'
+  root "users#index"
+  get "/users/", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
   # root 'pages/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
