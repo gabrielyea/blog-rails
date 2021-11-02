e17f683832a4572322a093702e2ee23131dc7cbe
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-02 10:12:23 -0600

    Remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
-  # root 'pages/hello'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
