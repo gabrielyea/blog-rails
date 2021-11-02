6fa778a6ce8342d4414b73a3367d462eae504b4e
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-02 09:55:28 -0600

    Generate post and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages/hello'
+  get 'posts/index'
+  get 'users/index'
+  # root 'pages/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
