336dd80bad0927f25c1c0d7d61a73e47022cd77d
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2021-11-04 12:13:28 -0600

    Update 20211104153225_create_posts.rb

--- db/migrate/20211104153225_create_posts.rb
+++ db/migrate/20211104153225_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.references :user, null: false, foreign_key: true
-      t.text :title
+      t.string :title
       t.text :text
       t.integer :comments_counter
       t.integer :likes_counter
