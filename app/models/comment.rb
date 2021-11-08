fc4eb518ae6524c2e761bc322f6d671d31f22aa1
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-08 14:59:38 -0600

    Add after_update callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_commit :update_post_comment_counter, on: :update
+
+  private
+
   def update_post_comment_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_commit :update_user_post_counter, on: :update
+
   def take_five_recent
     comments.order(created_at: :desc).limit(5)
   end
+
+  private
 
   def update_user_post_counter
     author.update(post_counter: author.posts.count)
