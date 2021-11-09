eb805b49b042e707972407dd7e3f15791a5bf0db
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-09 12:34:17 -0600

    Modify seed to generate more posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,23 @@ require 'faker'
       id: id, 
       name: Faker::Name.name,
       photo: Faker::Lorem.sentence,
-      bio: Faker::Lorem.sentence(word_count: 3, supplemental: true),
+      bio: Faker::Lorem.paragraph(sentence_count: 15, supplemental: true),
       post_counter: 0,
   )
 end
 
-(1..10).each do |id|
-  Post.create(
+(1..100).each do |id|
+  Post.create!(
       id: id,
       author_id: rand(1..20),
       title: Faker::Book.title,
-      text: Faker::Lorem.paragraph,
+      text: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true),
       comments_counter: 0,
       likes_counter: 0,
   )
 end
 
-(1..10).each do |id|
+(1..100).each do |id|
   Comment.create!(
       id: id,
       post_id: rand(1..10),
@@ -30,10 +30,21 @@ end
   )
 end
 
-(1..10).each do |id|
+(1..100).each do |id|
   Like.create!(
       id: id,
       author_id: rand(1..20),
       post_id: rand(1..10),
   )
+end
+
+User.all.each do |u|
+  u.post_counter = u.posts.count
+  u.save
+end
+
+Post.all.each do |p|
+  p.comments_counter = p.comments.count
+  p.likes_counter = p.likes.count
+  p.save
 end
