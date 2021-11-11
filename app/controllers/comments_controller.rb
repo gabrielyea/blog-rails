97e77e428f96ff38b2c8d6f1df552659d71a69c4
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-11 12:08:24 -0600

    Add comment save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,9 @@ class CommentsController < ApplicationController
     this_user = User.find(params[:user_id])
     this_post = this_user.posts.find(params[:post_id])
     @comment = Comment.new((params.require(:comment).permit(:text)))
-    # this_post.comments.new(params.require(:comment).permit(:text))
     @comment.post = this_post
     @comment.author = this_user
 
-
-    if @comment.save
-      redirect_to user_post_path(this_user, this_post)
-    end
-
+    redirect_to user_post_path(this_user, this_post) if @comment.save
   end
 end
