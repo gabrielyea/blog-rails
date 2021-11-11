370abc91420b64a6269746811c78143d8b86ed32
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-11 12:08:59 -0600

    Create like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,12 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_post_likes_counter
+  after_commit :update_post_likes_counter
 
   private
 
   def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+    p 'increment'
+    post.increment!(:likes_counter)
   end
 end
