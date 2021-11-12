1f3f84a0c627c2ed4952437629b18180f91b42f9
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-11 18:53:46 -0600

    Add validation to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  validates :text, presence: { message: 'Comment needs a message' }
   after_save :update_post_comment_counter
 
   private
