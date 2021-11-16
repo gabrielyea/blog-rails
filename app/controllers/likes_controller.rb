7645b23b0991c0d32ca97259a78b5184b53c98c3
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-15 19:16:29 -0600

    Add befor_action filter

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create]
+
   def create
-    user = User.find(params[:user_id])
-    post = user.posts.find(params[:id])
+    user = current_user
+    post = Post.find(params[:id])
+
     Like.create(author_id: current_user.id, post_id: post.id)
+
     if Like.exists?(author_id: current_user.id)
       flash[:notice] = 'Like saved!'
     else
       flash[:error] = 'Ups something went wrong!'
     end
     redirect_to user_post_path(user, post)
+  end
+
+  def already_liked?
+    Like.where(author_id: @user.id, id: @post.id).exists?
   end
 end
