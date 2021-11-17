218ba06b110d6fa4e76df2b860c21edb91af1e02
Gabriel <gabriel.gabrielyea@gmail.com>
2021-11-16 19:01:46 -0600

    Add callbacks for after delete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,10 +4,15 @@ class Comment < ApplicationRecord
 
   validates :text, presence: { message: 'Comment needs a message' }
   after_save :update_post_comment_counter
+  after_destroy :decrement_user_comment_counter
 
   private
 
   def update_post_comment_counter
     post.increment!(:comments_counter)
+  end
+
+  def decrement_user_comment_counter
+    post.decrement!(:comments_counter)
   end
 end
